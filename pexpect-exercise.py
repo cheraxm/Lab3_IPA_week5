fe1dbce6da32adbc911259d0ed3be9525a549ad9
Sahachindech Katedee <chin9908@gmail.com>
2024-09-04 10:37:05 +0700

    Completed pexpect-exercise.py

--- pexpect-exercise.py
+++ pexpect-exercise.py
@@ -20,14 +20,15 @@ child.sendline("ip add 172.16.1.1 255.255.255.255")
 child.expect(PROMPT)
 child.sendline("do sh ip int br")
 child.expect(PROMPT)
-result += child.before
+# result += child.before
 child.sendline("exit")
 child.expect(PROMPT)
-print(result)
-print()
-print(result.decode('UTF-8'))
+# print(result)
+# print()
+# print(result.decode('UTF-8'))
 child.sendline('exit')
-print("\n\nR1 Loopback setup completed!")
+print("Loopback0 172.16.1.1 is created on 172.31.115.3")
+
 
 result2 = b''
 child2 = pexpect.spawn('telnet 172.31.115.4')
@@ -44,11 +45,11 @@ child2.sendline("ip add 172.16.2.2 255.255.255.255")
 child2.expect(PROMPT)
 child2.sendline("do sh ip int br")
 child2.expect(PROMPT)
-result2 += child2.before
+# result2 += child2.before
 child2.sendline("exit")
 child2.expect(PROMPT)
-print(result2)
-print()
-print(result2.decode('UTF-8'))
+# print(result2)
+# print()
+# print(result2.decode('UTF-8'))
 child2.sendline('exit')
-print("\n\nR2 Loopback setup completed!")
+print("Loopback0 172.16.2.2 is created on 172.31.115.4")
